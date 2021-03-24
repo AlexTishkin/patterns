4f78a7bf673a8f1a139301fefd6aa3f96614ec1d
AlexTishkin <AlexTishkin57@yandex.ru>
2021-03-24 16:02:22 +0300

    Реализация паттерна Singleton / Одиночка

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Паттерн "Синглтон".
+    /// https://refactoring.guru/design-patterns/singleton
+    /// Вариант применения - инициализация настроек приложения и их хранение
+    /// </summary>
+    public class Singleton
+    {
+        // 1. Инициализация статичного приватного поля - экземпляра класса
+        private static Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+
+        // 2. Конструктор делаем приватным
+        private Singleton()
+        {
+            InitializeSettings();
+        }
+
+        // 3. Геттер  для получения экземпляра класса
+        public static Singleton Instance => _instance.Value;
+
+        // 4. Прочая логика (Хранение настроек приложения)
+        public string DatabaseConnection { get; private set; }
+        public bool IsDebug { get; private set; }
+
+        /// <summary>
+        /// Инициализация настроек откуда-нибудь (Например, из файла)
+        /// </summary>
+        private void InitializeSettings()
+        {
+            var json = File.ReadAllText("../../../settings.json");
+            var settings = JsonConvert.DeserializeObject<dynamic>(json);
+            DatabaseConnection = settings.DatabaseConnection;
+            IsDebug = settings.IsDebug;
+        }
+    }
+}
---
+++ Singleton/SingletonExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class SingletonExample
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.Instance;
+
+            var isDebug = singleton.IsDebug;
+            var dbConnection = singleton.DatabaseConnection;
+
+            Console.WriteLine(isDebug ? "Debug mode" : "Release mode");
+            Console.WriteLine("Databace connection is " + dbConnection);
+
+        }
+    }
+}
